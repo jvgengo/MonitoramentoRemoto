5c4589d37916a0a40f8e6bb4b6c7b1859f6f63fa
jvgengo <jvgengo@gmail.com>
2014-10-09 09:34:14 -0300

    exercicio verifica cabo

---
+++ src/exeverificacabo/Main.java
@@ -0,0 +1,10 @@
+package exeverificacabo;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Verificadora v = new Verificadora();
+		v.iniciarVerificadora();
+	}
+}
---
+++ src/exeverificacabo/Verificadora.java
@@ -0,0 +1,77 @@
+package exeverificacabo;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class Verificadora implements Runnable{
+
+	private Thread tVerifica;
+	private NetworkInterface ni;
+	private boolean rodando;
+	private Log log;
+	private String usuario;
+	private String ip;
+	
+	private boolean desconectou = false;
+	private boolean desconectadoAntes = false;
+	
+	public Verificadora() {
+		
+		tVerifica = new Thread(this);
+		rodando = false;
+		log = new Log();
+		
+		try { 
+			ni = NetworkInterface.getByName("eth3");
+		} catch (SocketException e) {
+			System.err.println("Erro na NetworkInterface");
+		}
+		
+		usuario = System.getProperty("user.name");
+		
+		try {
+			InetAddress inet = InetAddress.getLocalHost();
+			ip = inet.getHostAddress();
+		} catch (UnknownHostException e) {
+			System.err.println("Erro ao pegar o Ip");
+		}
+		
+	}
+	
+	public void iniciarVerificadora() {
+		rodando = true;
+		tVerifica.start();
+	}
+	
+	private boolean verificaCabo() throws SocketException, InterruptedException {
+		desconectadoAntes = desconectou;//guarda estado anterior 
+		desconectou = !ni.isUp();		//fica true se não estiver conectado
+		Thread.sleep(1000);
+		return ni.isUp();
+	}
+	
+	public void pararVerificadora() {
+		rodando = false;
+	}
+	
+	@Override
+	public void run(){
+		while(rodando) {
+			try {
+				if (!verificaCabo())
+					if (desconectadoAntes!=desconectou)	//se o estado atual diferente do anterior e se o estado atual = desconectado
+							log.notificar(usuario,ip);
+			} catch (IOException e) {
+				e.getStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+		
+}
